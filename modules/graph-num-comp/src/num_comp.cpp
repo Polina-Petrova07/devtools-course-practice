4da2bfc4e4e80d68aba2d1ff019fd93b0fba448e
Polina-Petrova07 <polina_petrova_02@bk.ru>
2022-04-17 14:08:48 +0300

    add number of components algorithm

--- modules/graph-num-comp/src/num_comp.cpp
+++ modules/graph-num-comp/src/num_comp.cpp
@@ -58,3 +58,22 @@ void matrixType::DFS(int start, int f)
 		if (!used[i] && (this->Graph[start][i] != 0))
 			DFS(i, f);
 }
+int matrixType::numComp(int f)
+{
+	if (f != 0)
+	{
+		for (int i = 0; i < this->numVert; i++)
+			this->used.push_back(false);
+		f = 0;
+	}
+	int n = 0;
+	for (int i = 0; i < this->numVert; i++)
+	{
+		if (!used[i])
+		{
+			++n;
+			matrixType::DFS(i, f);
+		}
+	}
+	return n;
+}
